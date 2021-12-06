b19f631954d5386c30b645704b3aa7da83e15ef2
Georges Le Bellier <glebellier@protonmail.com>
2021-12-06 12:23:54 +0100

    Add length of dataset items size

--- tests/test_dataset.py
+++ tests/test_dataset.py
@@ -1,5 +1,6 @@
 import pytest
 import numpy as np
+from sklearn.preprocessing import StandardScaler, MinMaxScaler
 
 from perf_gan.data.make_dataset import DatasetCreator
 from perf_gan.data.dataset import GANDataset
@@ -12,8 +13,30 @@ from perf_gan.data.dataset import GANDataset
         (1024, 2, 5),
     ],
 )
-def test_model(sr, n, duration):
+def test_dataset_creation(sr: int, n: int, duration: int):
+    """Check dataset creation
+
+    Args:
+        sr (int): sampling rate
+        n (int): number of samples in the dataset
+        duration (int): length of each sample
+    """
     data = DatasetCreator(sr=sr)
     data.build(n, duration)
     print(data.e_f0)
     assert len(data.e_f0) == n * duration * sr
+
+
+def test_dataset_items():
+    """Test size of dataset items
+    """
+    l = [(StandardScaler, {}), (MinMaxScaler, {})]
+    for size in [2048, 1024, 512]:
+        d = GANDataset(path="data/dataset.pickle",
+                       n_sample=size,
+                       list_transforms=l)
+        d.transform()
+        # loop over the 4 components (u contours, e contours, onsets, offsets)
+        item = d[0]
+        for idx in range(4):
+            assert len(item[idx]) == size
