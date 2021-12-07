dedd80d46b5a901978ab1c4577d3c93955de048b
Georges Le Bellier <glebellier@protonmail.com>
2021-12-07 21:12:29 +0100

    Add the first pitch loss

--- src/perf_gan/models/pitch_loss.py
+++ src/perf_gan/models/pitch_loss.py
@@ -3,19 +3,28 @@ import torch.nn as nn
 
 
 class PitchLoss(nn.Module):
-    def __init__(self):
+    def __init__(self, threshold: .5):
         super().__init(self)
+        self.threshold = threshold
 
-    def forward(self, x):
+    def forward(self, x: list[torch.Tensor]) -> torch.Tensor:
+
+        # create the corresponding mask
         mask = self.__create_mask(x)
 
         u_f0, _ = x[0].split(1, -1)
         e_f0, _ = x[0].split(1, -1)
 
         # apply mask to the pitch contours
-
         mk_u_f0 = mask * u_f0
         mk_e_f0 = mask * e_f0
+
+        # compute the midi pitch differences
+        diff = torch.abs(mk_e_f0 - mk_u_f0)
+        # proportion of contours points where pitch is wrong (diff above threshold)
+        loss = torch.mean((diff > self.threshold).float()) / torch.mean(mask)
+
+        return loss
 
     def __create_mask(x: list[torch.Tensor]) -> torch.Tensor:
         """Create a temporal mask according to notes onsets and offsets.
