115a0f6920e143bb2a449507401717f750e020aa
Georges Le Bellier <glebellier@protonmail.com>
2021-12-08 10:50:43 +0100

    Add losses from  lsgan article

---
+++ src/perf_gan/losses/lsgan_loss.py
@@ -0,0 +1,36 @@
+import torch
+
+
+class LSGAN_loss:
+    def __init__(self):
+        pass
+
+    def disc_loss(self, dx: torch.Tensor, dgz: torch.Tensor) -> torch.Tensor:
+        """Compute the discriminator loss in the LSGAN fashion
+
+        Args:
+            dx (torch.Tensor): output of the discriminator for real samples
+            dgz (torch.Tensor): output of the discriminator for generated samples
+
+        Returns:
+            torch.Tensor: loss of the discriminator 
+        """
+
+        loss_fake = 0.5 * torch.mean((dx - 1)**2)
+        loss_real = 0.5 * torch.mean(dgz**2)
+
+        return loss_fake + loss_real
+
+    def gen_loss(self, dx, dgz):
+        """Compute the generator loss in the LSGAN fashion
+
+        Args:
+            dx (torch.Tensor): output of the discriminator for real samples
+            dgz (torch.Tensor): output of the discriminator for generated samples
+
+        Returns:
+            torch.Tensor: loss of the generator 
+        """
+
+        loss_gen = 0.5 * torch.mean((dgz - 1)**2)
+        return loss_gen
