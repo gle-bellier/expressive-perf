50137226ba6657c46a399388cf6b3c90d8a9ace7
Georges Le Bellier <glebellier@protonmail.com>
2022-02-01 18:42:06 +0100

    Minor changes

--- src/perf_gan/data/extract_descriptors.py
+++ src/perf_gan/data/extract_descriptors.py
@@ -1,26 +1,40 @@
 import librosa as li
+import numpy as np
 import torch
+
+from typing import Union
 from perf_gan.data.descriptors import extract_lo, extract_f0
 
 
 class Extractor:
-    def __init__(self, filename, sr=16000, block_size=160):
+    """Contours extraction tools 
+    """
+    def __init__(self, filename: str, sr=16000, block_size=160) -> None:
+        """Initialize extractor tool.
+
+        Args:
+            filename (str): complete path to the audio file
+            sr (int, optional): sampling rate. Defaults to 16000.
+            block_size (int, optional): window size for f0 and loudness computing. Defaults to 160.
+        """
         self.filename = filename
         self.sr = sr
         self.block_size = block_size
         self.ddsp = torch.jit.load("ddsp_flute.ts").eval()
 
-    def extract_f0_lo(self):
+    def extract_f0_lo(self) -> Union(torch.Tensor, torch.Tensor):
         audio, fs = li.load(self.filename, sr=self.sr)
-        print("Audio shape : ", audio.shape)
+
         lo = extract_lo(audio, self.sr, self.block_size)
         f0 = extract_f0(audio, self.sr, self.block_size)
 
         return f0, lo
 
-    def reconstruct(self, f0, lo):
+    def reconstruct(self, f0: torch.Tensor, lo: torch.Tensor) -> np.ndarray:
         f0 = f0.reshape(1, -1, 1)
         lo = lo.reshape(1, -1, 1)
+
+        # reconstruct signal with ddsp
         signal = self.ddsp(f0, lo)
         return signal.reshape(-1).cpu().numpy()
 
