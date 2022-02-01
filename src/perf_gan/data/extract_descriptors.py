95856692549cee83801f022425f30de44fe7478b
Georges Le Bellier <glebellier@protonmail.com>
2022-02-01 19:05:34 +0100

    Update docstring

--- src/perf_gan/data/extract_descriptors.py
+++ src/perf_gan/data/extract_descriptors.py
@@ -15,11 +15,10 @@ from perf_gan.data.descriptors import extract_lo, extract_f0
 class Extractor:
     """Contours extraction tools 
     """
-    def __init__(self, filename: str, sr=16000, block_size=160) -> None:
+    def __init__(self, sr=16000, block_size=160) -> None:
         """Initialize extractor tool.
 
         Args:
-            filename (str): complete path to the audio file
             sr (int, optional): sampling rate. Defaults to 16000.
             block_size (int, optional): window size for f0 and loudness computing. Defaults to 160.
         """
@@ -28,23 +27,45 @@ class Extractor:
         self.block_size = block_size
         self.ddsp = torch.jit.load("ddsp_flute.ts").eval()
 
-    def extract_f0_lo(self, audio):
+    def extract_f0_lo(
+            self, audio: torch.Tensor) -> Union[torch.Tensor, torch.Tensor]:
+
         lo = extract_lo(audio, self.sr, self.block_size)
         f0 = extract_f0(audio, self.sr, self.block_size)
 
         return f0, lo
 
     def reconstruct(self, f0: torch.Tensor, lo: torch.Tensor) -> np.ndarray:
+        """Reconstruct sound with DDSP model according to input f0 and loudness contours
+
+        Args:
+            f0 (torch.Tensor): fundamental frequency contour
+            lo (torch.Tensor): loudness contour
+
+        Returns:
+            np.ndarray: reconstructed waveform
+        """
         f0 = f0.reshape(1, -1, 1)
         lo = lo.reshape(1, -1, 1)
+
+        # synth signal with ddsp
         signal = self.ddsp(torch.tensor(f0).float(), torch.tensor(lo).float())
         return signal.reshape(-1).detach()
 
-    def multi_scale_loss(self, x, resynth, overlap=0.75, alpha=.5):
+    def multi_scale_loss(self,
+                         x: torch.Tensor,
+                         resynth: np.ndarray,
+                         overlap=0.75,
+                         alpha=.5) -> torch.Tensor:
+
         loss = 0
         x = x.squeeze()
         resynth = resynth.squeeze()
+
+        # select some window sizes for analysis
         fft_sizes = [1024, 512, 256, 128, 64]
+
+        # compute score for each fft window size
         for fft_size in fft_sizes:
             hop = int(fft_size * (1 - overlap))
             window = torch.hann_window(fft_size).to(x)
@@ -69,7 +90,21 @@ class Extractor:
                              torch.log(y_stft + 1e-7)).abs().mean()
         return loss
 
-    def scan(self, x, resynth, window_size=2048):
+    def scan(self,
+             x: torch.Tensor,
+             resynth: np.ndarray,
+             window_size=2048) -> list:
+        """Compute the multi_scale_loss for each chunks of the current audio
+
+        Args:
+            x (torch.Tensor): original waveform
+            resynth (np.ndarray): reconstructed waveform
+            window_size (int, optional): size of each audio chunks to analyze. Defaults to 2048.
+
+        Returns:
+            list: list of the multi-scale loss each audio chunks in the sample
+        """
+
         # we need to crop the initial sample
         # since it can be shortened during extraction
         # and reconstruction time
@@ -84,7 +119,23 @@ class Extractor:
 
         return rslt
 
-    def select(self, x, samples_size=2048, ratio=0.10):
+    def select(self,
+               x: np.ndarray,
+               samples_size=2048,
+               ratio=0.80) -> Union[torch.Tensor, torch.Tensor]:
+        """ Select the f0 and loudness contours of the best chunks of the 
+        input audio according to a multi-scale spectral loss computed on the
+        reconstructed audio with DDSP.
+
+        Args:
+            x (np.ndarray): input audio, raw waveform.
+            samples_size (int, optional): size of each audio chunks under study. Defaults to 2048.
+            ratio (float, optional): ratio of audio chunks to keep, e.g 0.7 means we 
+            keep around 70% of the original audio. Defaults to 0.80.
+
+        Returns:
+            Union[torch.Tensor, torch.Tensor]: f0 and loudness contours
+        """
 
         f0, lo = self.extract_f0_lo(x)
         resynth = self.reconstruct(f0, lo)
