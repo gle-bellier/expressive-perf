5dabec84386334758b71ecce49a6554e1a898d52
Georges Le Bellier <glebellier@protonmail.com>
2022-03-11 12:20:25 +0100

    Add generator and discriminator

--- src/perf_gan/models/exp_time_gan/generator.py
+++ src/perf_gan/models/exp_time_gan/generator.py
@@ -1,7 +1,46 @@
 import torch
 import torch.nn as nn
 
+from perf_gan.models.blocks.conv_blocks import ConvBlock
+
+
+class GeneratorBlock(nn.Module):
+    def __init__(self, in_c, out_c, dilation, dropout):
+        super(GeneratorBlock, self).__init__()
+
+        self.in_conv = ConvBlock(in_c, in_c, dilation, dropout)
+        self.out_conv = ConvBlock(in_c, out_c, dilation, dropout)
+
+        self.avg = nn.AvgPool1d(2)
+
+    def forward(self, x):
+        x = self.in_conv(x)
+        x = self.avg(x)
+        x = self.out_conv(x)
+        return x
+
 
 class Generator(nn.Module):
-    def __init__(self):
-        pass
+    def __init__(self, channels, dropout=0):
+
+        super(Generator, self).__init__()
+
+        self.channels = channels
+        self.encoder = nn.ModuleList([
+            GeneratorBlock(in_c, out_c, dilation=1, dropout=dropout)
+            for in_c, out_c in zip(channels[:-1], channels[1:])
+        ])
+
+    def forward(self, x):
+
+        for block in self.encoder:
+            x = block(x)
+            print(x.shape)
+
+        return x
+
+
+if __name__ == "__main__":
+    x = torch.randn(32, 2, 1024)
+    g = Generator([2, 64, 128, 512, 1024])
+    g(x)
