d919c264f10e74c2e398ca090109bae062dfb6dc
Georges Le Bellier <glebellier@protonmail.com>
2022-03-11 15:36:34 +0100

    Add autoencoder model for contours reconstruction

---
+++ src/perf_gan/models/exp_time_gan/ae.py
@@ -0,0 +1,24 @@
+import torch
+import torch.nn as nn
+
+from perf_gan.models.exp_time_gan.encoder import Encoder
+from perf_gan.models.exp_time_gan.decoder import Decoder
+
+
+class AutoEncoder(nn.Module):
+
+    def __init__(self, channels, dropout):
+        super(AutoEncoder, self).__init__()
+
+        self.encoder = Encoder(channels, dropout=dropout)
+        self.decoder = Decoder(channels[::-1], dropout=dropout)
+
+    def encode(self, x):
+        return self.encoder(x)
+
+    def decode(self, x):
+        return self.decoder(x)
+
+    def forward(self, x):
+        x = self.encode(x)
+        return self.decode(x)
